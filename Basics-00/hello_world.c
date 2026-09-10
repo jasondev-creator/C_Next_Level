fd69eab1e1a30e3067279dfc536a0127f8bde849
jasondev-creator <jasonstudy610@gmail.com>
2026-09-10 16:44:30 +0530

    update hello_world.c

---
+++ Basics-00/hello_world.c
@@ -0,0 +1,24 @@
+/*
+ * Program: Basic Structure of a C Program
+ *
+ * Concepts:
+ * - Header file
+ * - main() function
+ * - printf()
+ * - return statement
+ *
+ * Approach:
+ * 1. Include the required header file.
+ * 2. Execution begins from main().
+ * 3. Display a message using printf().
+ * 4. Return 0 to indicate successful execution.
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello, World!\n");
+
+    return 0;
+}
