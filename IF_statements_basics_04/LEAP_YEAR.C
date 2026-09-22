714ff4a8d7f494c86d7125e4c8bb0dac6fa861d2
N.Jason Augustin <jasonstudy610@gmail.com>
2026-09-22 13:03:20 +0530

    Update LEAP_YEAR.C

--- IF_statements_basics_04/LEAP_YEAR.C
+++ IF_statements_basics_04/LEAP_YEAR.C
@@ -3,7 +3,7 @@ int main () {
 int leap_year;
 printf("Enter the year:\n");
 scanf("%d",&leap_year);
-if(leap_year % 4 == 0 || leap_year %400 == 0 &&leap_year % 100 != 0)
+if(leap_year % 4 == 0 || leap_year % 400 == 0 && leap_year % 100 != 0)
 {
 printf("Leap year.\n");
 }
